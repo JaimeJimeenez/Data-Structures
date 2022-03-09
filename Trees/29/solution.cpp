085921c0316a2befe5258eafa1a60b69d3851ac5
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-09 19:14:23 +0100

    Exercise 29 corrected by judge

---
+++ Trees/29/solution.cpp
@@ -0,0 +1,186 @@
+/*
+ * ---------------------------------------------------
+ *                ESTRUCTURAS DE DATOS
+ * ---------------------------------------------------
+ *              Facultad de Informática
+ *         Universidad Complutense de Madrid
+ * ---------------------------------------------------
+ */
+ 
+/*
+ * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
+ * modificar el código contenido entre las etiquetas <answer>
+ * y </answer>. Toda modificación fuera de esas etiquetas está
+ * prohibida, pues no se tendrá en cuenta para la corrección.
+ *
+ * Tampoco esta permitido modificar las líneas que contienen
+ * las etiquetas <answer> y </answer>, obviamente :-)
+ */
+
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <memory>
+#include <utility>
+#include <tuple>
+#include <algorithm>
+
+template <class T> class BinTree {
+public:
+  BinTree() : root_node(nullptr) {}
+
+  BinTree(const T &elem)
+      : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
+
+  BinTree(const BinTree &left, const T &elem, const BinTree &right)
+      : root_node(std::make_shared<TreeNode>(left.root_node, elem,
+                                             right.root_node)) {}
+
+  bool empty() const { return root_node == nullptr; }
+
+  const T &root() const {
+    assert(root_node != nullptr);
+    return root_node->elem;
+  }
+
+  BinTree left() const {
+    assert(root_node != nullptr);
+    BinTree result;
+    result.root_node = root_node->left;
+    return result;
+  }
+
+  BinTree right() const {
+    assert(root_node != nullptr);
+    BinTree result;
+    result.root_node = root_node->right;
+    return result;
+  }
+
+  void display(std::ostream &out) const { display_node(root_node, out); }
+  
+  int size() const { return size(this->root_node); }
+
+  int height() const { return height(this->root_node); }
+
+  int children() const { return children(this->root_node); }
+
+private:
+  // Las definiciones de TreeNode y NodePointer dependen recursivamente
+  // la una de la otra. Por eso declaro 'struct TreeNode;' antes de NodePointer
+  // para que el compilador sepa, cuando analice la definición de NodePointer,
+  // que TreeNode va a ser definida más adelante.
+
+  struct TreeNode;
+  using NodePointer = std::shared_ptr<TreeNode>;
+
+  struct TreeNode {
+    TreeNode(const NodePointer &left, const T &elem, const NodePointer &right)
+        : elem(elem), left(left), right(right) {}
+
+    T elem;
+    NodePointer left, right;
+  };
+
+  NodePointer root_node;
+
+  static void display_node(const NodePointer &root, std::ostream &out) {
+    if (root == nullptr) {
+      out << ".";
+    } else {
+      out << "(";
+      display_node(root->left, out);
+      out << " " << root->elem << " ";
+      display_node(root->right, out);
+      out << ")";
+    }
+  }
+
+  int size(const NodePointer &root) const {
+    if (root == nullptr)
+        return 0;
+    return 1 + size(root->left) + size(root->right);
+  }
+
+  int height(const NodePointer &root) const {
+    if (root == nullptr)
+        return 0;
+    
+    return 1 + std::max(height(root->right), height(root->left));
+  }
+
+  int children(const NodePointer &root) const {
+    if (root == nullptr)
+        return 0;
+    else if (root->right == nullptr && root->left == nullptr)
+        return 1;
+    else
+        return children(root->right) + children(root->left); 
+  }
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const BinTree<T> &tree) {
+  tree.display(out);
+  return out;
+}
+
+template <typename T> BinTree<T> read_tree(std::istream &in) {
+  char c;
+  in >> c;
+  if (c == '.') {
+    return BinTree<T>();
+  } else {
+    assert(c == '(');
+    BinTree<T> left = read_tree<T>(in);
+    T elem;
+    in >> elem;
+    BinTree<T> right = read_tree<T>(in);
+    in >> c;
+    assert(c == ')');
+    BinTree<T> result(left, elem, right);
+    return result;
+  }
+}
+
+using namespace std;
+
+//@ <answer>
+//---------------------------------------------------------------
+// Modificar a partir de aquí
+// --------------------------------------------------------------
+
+
+// Función que trata un caso de prueba
+void tratar_caso() {
+  BinTree<char> t = read_tree<char>(cin);
+  cout << t.size() << " " << t.children() << " " << t.height() << endl;
+}
+
+
+//---------------------------------------------------------------
+// No modificar por debajo de esta línea
+// --------------------------------------------------------------
+//@ </answer>
+
+
+int main() {
+#ifndef DOMJUDGE
+  std::ifstream in("sample.in");
+  auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+  int num_casos;
+  cin >> num_casos;
+  
+  for (int i = 0; i < num_casos; i++) {
+    tratar_caso();
+  }
+
+#ifndef DOMJUDGE
+  std::cin.rdbuf(cinbuf);
+  // Descomentar si se trabaja en Windows
+  // system("PAUSE");
+#endif
+  return 0;
+}
+
