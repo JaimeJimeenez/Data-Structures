ece8adc97906ace62884cbdb5e899cdeefa0d08d
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-12 12:27:16 +0100

    Exercise 6.1 corrrected by judge

--- Trees/6/1/solution.cpp
+++ Trees/6/1/solution.cpp
@@ -6,25 +6,25 @@
  *         Universidad Complutense de Madrid
  * ---------------------------------------------------
  */
- 
-/*
- * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
- * modificar el código contenido entre las etiquetas <answer>
- * y </answer>. Toda modificación fuera de esas etiquetas está
- * prohibida, pues no se tendrá en cuenta para la corrección.
- *
- * Tampoco esta permitido modificar las líneas que contienen
- * las etiquetas <answer> y </answer>, obviamente :-)
- */
- 
-//@ <answer>
-/*
-  Introduce aquí los nombres de los componentes del grupo:
-  
-  Componente 1:
-  Componente 2:
-*/
-//@ </answer>
+
+ /*
+  * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
+  * modificar el código contenido entre las etiquetas <answer>
+  * y </answer>. Toda modificación fuera de esas etiquetas está
+  * prohibida, pues no se tendrá en cuenta para la corrección.
+  *
+  * Tampoco esta permitido modificar las líneas que contienen
+  * las etiquetas <answer> y </answer>, obviamente :-)
+  */
+
+  //@ <answer>
+  /*
+    Introduce aquí los nombres de los componentes del grupo:
+
+    Componente 1: Jaime Jiménez Nieto
+    Componente 2: Iván Pisonero Díaz
+  */
+  //@ </answer>
 
 #include <iostream>
 #include <cassert>
@@ -36,92 +36,94 @@
 
 template <class T> class BinTree {
 public:
-  BinTree() : root_node(nullptr) {}
+    BinTree() : root_node(nullptr) {}
 
-  BinTree(const T &elem)
-      : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
+    BinTree(const T& elem)
+        : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
 
-  BinTree(const BinTree &left, const T &elem, const BinTree &right)
-      : root_node(std::make_shared<TreeNode>(left.root_node, elem,
-                                             right.root_node)) {}
+    BinTree(const BinTree& left, const T& elem, const BinTree& right)
+        : root_node(std::make_shared<TreeNode>(left.root_node, elem,
+            right.root_node)) {}
 
-  bool empty() const { return root_node == nullptr; }
+    bool empty() const { return root_node == nullptr; }
 
-  const T &root() const {
-    assert(root_node != nullptr);
-    return root_node->elem;
-  }
+    const T& root() const {
+        assert(root_node != nullptr);
+        return root_node->elem;
+    }
 
-  BinTree left() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->left;
-    return result;
-  }
+    BinTree left() const {
+        assert(root_node != nullptr);
+        BinTree result;
+        result.root_node = root_node->left;
+        return result;
+    }
 
-  BinTree right() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->right;
-    return result;
-  }
+    BinTree right() const {
+        assert(root_node != nullptr);
+        BinTree result;
+        result.root_node = root_node->right;
+        return result;
+    }
 
-  void display(std::ostream &out) const { display_node(root_node, out); }
+    void display(std::ostream& out) const { display_node(root_node, out); }
 
 private:
-  // Las definiciones de TreeNode y NodePointer dependen recursivamente
-  // la una de la otra. Por eso declaro 'struct TreeNode;' antes de NodePointer
-  // para que el compilador sepa, cuando analice la definición de NodePointer,
-  // que TreeNode va a ser definida más adelante.
+    // Las definiciones de TreeNode y NodePointer dependen recursivamente
+    // la una de la otra. Por eso declaro 'struct TreeNode;' antes de NodePointer
+    // para que el compilador sepa, cuando analice la definición de NodePointer,
+    // que TreeNode va a ser definida más adelante.
 
-  struct TreeNode;
-  using NodePointer = std::shared_ptr<TreeNode>;
+    struct TreeNode;
+    using NodePointer = std::shared_ptr<TreeNode>;
 
-  struct TreeNode {
-    TreeNode(const NodePointer &left, const T &elem, const NodePointer &right)
-        : elem(elem), left(left), right(right) {}
+    struct TreeNode {
+        TreeNode(const NodePointer& left, const T& elem, const NodePointer& right)
+            : elem(elem), left(left), right(right) {}
 
-    T elem;
-    NodePointer left, right;
-  };
+        T elem;
+        NodePointer left, right;
+    };
 
-  NodePointer root_node;
+    NodePointer root_node;
 
-  static void display_node(const NodePointer &root, std::ostream &out) {
-    if (root == nullptr) {
-      out << ".";
-    } else {
-      out << "(";
-      display_node(root->left, out);
-      out << " " << root->elem << " ";
-      display_node(root->right, out);
-      out << ")";
+    static void display_node(const NodePointer& root, std::ostream& out) {
+        if (root == nullptr) {
+            out << ".";
+        }
+        else {
+            out << "(";
+            display_node(root->left, out);
+            out << " " << root->elem << " ";
+            display_node(root->right, out);
+            out << ")";
+        }
     }
-  }
 };
 
 template <typename T>
-std::ostream &operator<<(std::ostream &out, const BinTree<T> &tree) {
-  tree.display(out);
-  return out;
+std::ostream& operator<<(std::ostream& out, const BinTree<T>& tree) {
+    tree.display(out);
+    return out;
 }
 
-template <typename T> BinTree<T> read_tree(std::istream &in) {
-  char c;
-  in >> c;
-  if (c == '.') {
-    return BinTree<T>();
-  } else {
-    assert(c == '(');
-    BinTree<T> left = read_tree<T>(in);
-    T elem;
-    in >> elem;
-    BinTree<T> right = read_tree<T>(in);
+template <typename T> BinTree<T> read_tree(std::istream& in) {
+    char c;
     in >> c;
-    assert(c == ')');
-    BinTree<T> result(left, elem, right);
-    return result;
-  }
+    if (c == '.') {
+        return BinTree<T>();
+    }
+    else {
+        assert(c == '(');
+        BinTree<T> left = read_tree<T>(in);
+        T elem;
+        in >> elem;
+        BinTree<T> right = read_tree<T>(in);
+        in >> c;
+        assert(c == ')');
+        BinTree<T> result(left, elem, right);
+        return result;
+    }
 }
 
 using namespace std;
@@ -131,36 +133,38 @@ using namespace std;
 // Modificar a partir de aquí
 // --------------------------------------------------------------
 
-template<typename T>
-int altura(const BinTree<T> &arbol) {
-  if (arbol.empty())
-    return 0;
-  return 1 + max(altura(arbol.left()), altura(arbol.right()));
+//Coste O(n) siendo n el número de elementos del arbol
+template <typename T>
+std::pair<bool, int>estableEnAltura_altura(const BinTree<T>& arbol) {
+    if (arbol.empty())
+        return { true, 0 };
+    if (arbol.left().empty() && arbol.right().empty())
+        return { false, 1 };
+
+    auto [estable_dr, alt_dr] = estableEnAltura_altura(arbol.right());
+    auto [estable_iz, alt_iz] = estableEnAltura_altura(arbol.left());
+
+    if (alt_dr == alt_iz) {
+        return{ true, 1 + alt_dr };
+    }
+    else if (alt_dr > alt_iz) {
+        return{ estable_dr, 1 + alt_dr };
+    }
+    else {
+        return{ estable_iz, 1 + alt_iz };
+    }
 }
 
-// No olvides el coste!
+// Coste lineal con respecto al número de nodos de arbol
 template <typename T>
-bool estable_altura(const BinTree<T> &arbol) {
-  if (arbol.empty())
-    return true;
-
-  int alturaIzq = altura(arbol.left());
-  int alturaDer = altura(arbol.right());
-
-  if (arbol.left().empty() || arbol.right().empty())
-    return false;
-  if (alturaIzq > alturaDer)
-    return estable_altura(arbol.left());
-  else if (alturaDer > alturaIzq)
-    return estable_altura(arbol.right());
-
-  return true;
+bool estable_altura(const BinTree<T>& arbol) {
+    return estableEnAltura_altura(arbol).first;
 }
 
 // Función para tratar un caso de prueba
 void tratar_caso() {
-  BinTree<int> t = read_tree<int>(cin);
-  cout << (estable_altura(t) ? "SI" : "NO") << "\n";
+    BinTree<int> t = read_tree<int>(cin);
+    cout << (estable_altura(t) ? "SI" : "NO") << "\n";
 }
 
 
@@ -172,21 +176,20 @@ void tratar_caso() {
 
 int main() {
 #ifndef DOMJUDGE
-  std::ifstream in("sample.in");
-  auto cinbuf = std::cin.rdbuf(in.rdbuf());
+    std::ifstream in("sample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
-  int num_casos;
-  cin >> num_casos;
-  
-  for (int i = 0; i < num_casos; i++) {
-    tratar_caso();
-  }
+    int num_casos;
+    cin >> num_casos;
+
+    for (int i = 0; i < num_casos; i++) {
+        tratar_caso();
+    }
 
 #ifndef DOMJUDGE
-  std::cin.rdbuf(cinbuf);
-  // Descomentar si se trabaja en Windows
-  // system("PAUSE");
+    std::cin.rdbuf(cinbuf);
+    // Descomentar si se trabaja en Windows
+    // system("PAUSE");
 #endif
-  return 0;
+    return 0;
 }
-
