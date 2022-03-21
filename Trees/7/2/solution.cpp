60190aaa14606ccb255b8d11feece654e377da2f
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-21 21:19:21 +0100

    Exercise 7.2 corrected by judge

--- Trees/7/2/solution.cpp
+++ Trees/7/2/solution.cpp
@@ -2,29 +2,29 @@
  * ---------------------------------------------------
  *                ESTRUCTURAS DE DATOS
  * ---------------------------------------------------
- *              Facultad de Informática
+ *              Facultad de Inform�tica
  *         Universidad Complutense de Madrid
  * ---------------------------------------------------
  */
- 
-/*
- * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
- * modificar el código contenido entre las etiquetas <answer>
- * y </answer>. Toda modificación fuera de esas etiquetas está
- * prohibida, pues no se tendrá en cuenta para la corrección.
- *
- * Tampoco esta permitido modificar las líneas que contienen
- * las etiquetas <answer> y </answer>, obviamente :-)
- */
- 
-//@ <answer>
-/*
-  Introduce aquí los nombres de los componentes del grupo:
-  
-  Componente 1: Jaime Jiménez Nieto
-  Componente 2: Ivan Pisonero Díaz
-*/
-//@ </answer>
+
+ /*
+  * MUY IMPORTANTE: Para realizar este ejercicio solo pod�is
+  * modificar el c�digo contenido entre las etiquetas <answer>
+  * y </answer>. Toda modificaci�n fuera de esas etiquetas est�
+  * prohibida, pues no se tendr� en cuenta para la correcci�n.
+  *
+  * Tampoco esta permitido modificar las l�neas que contienen
+  * las etiquetas <answer> y </answer>, obviamente :-)
+  */
+
+  //@ <answer>
+  /*
+    Introduce aqu� los nombres de los componentes del grupo:
+
+    Componente 1: Jaime Jim�nez Nieto
+    Componente 2: Iv�n Pisonero D�az
+  */
+  //@ </answer>
 
 #include <iostream>
 #include <cassert>
@@ -34,229 +34,238 @@
 #include <utility>
 #include <limits>
 #include <tuple>
-#include <algorithm> 
+
 
 template <class T> class BinTree {
 
 private:
-  struct TreeNode;
-  using NodePointer = std::shared_ptr<TreeNode>;
+    struct TreeNode;
+    using NodePointer = std::shared_ptr<TreeNode>;
 
 public:
-  BinTree() : root_node(nullptr) {}
+    BinTree() : root_node(nullptr) {}
 
-  BinTree(const T &elem)
-      : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
+    BinTree(const T& elem)
+        : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
 
-  BinTree(const BinTree &left, const T &elem, const BinTree &right)
-      : root_node(std::make_shared<TreeNode>(left.root_node, elem,
-                                             right.root_node)) {}
+    BinTree(const BinTree& left, const T& elem, const BinTree& right)
+        : root_node(std::make_shared<TreeNode>(left.root_node, elem,
+            right.root_node)) {}
 
-  bool empty() const { return root_node == nullptr; }
+    bool empty() const { return root_node == nullptr; }
 
-  const T &root() const {
-    assert(root_node != nullptr);
-    return root_node->elem;
-  }
+    const T& root() const {
+        assert(root_node != nullptr);
+        return root_node->elem;
+    }
 
-  BinTree left() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->left;
-    return result;
-  }
+    BinTree left() const {
+        assert(root_node != nullptr);
+        BinTree result;
+        result.root_node = root_node->left;
+        return result;
+    }
 
-  BinTree right() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->right;
-    return result;
-  }
+    BinTree right() const {
+        assert(root_node != nullptr);
+        BinTree result;
+        result.root_node = root_node->right;
+        return result;
+    }
 
-  void display(std::ostream &out) const { display_node(root_node, out); }
+    void display(std::ostream& out) const { display_node(root_node, out); }
 
-  template <typename U> void preorder(U func) const {
-    preorder(root_node, func);
-  }
+    template <typename U> void preorder(U func) const {
+        preorder(root_node, func);
+    }
 
-  template <typename U> void inorder(U func) const { inorder(root_node, func); }
+    template <typename U> void inorder(U func) const { inorder(root_node, func); }
 
-  template <typename U> void postorder(U func) const {
-    postorder(root_node, func);
-  }
+    template <typename U> void postorder(U func) const {
+        postorder(root_node, func);
+    }
 
-  template <typename U> void levelorder(U func) const;
+    template <typename U> void levelorder(U func) const;
 
 private:
-  struct TreeNode {
-    TreeNode(const NodePointer &left, const T &elem, const NodePointer &right)
-        : elem(elem), left(left), right(right) {}
+    struct TreeNode {
+        TreeNode(const NodePointer& left, const T& elem, const NodePointer& right)
+            : elem(elem), left(left), right(right) {}
 
-    T elem;
-    NodePointer left, right;
-  };
+        T elem;
+        NodePointer left, right;
+    };
 
-  NodePointer root_node;
+    NodePointer root_node;
 
-  static void display_node(const NodePointer &root, std::ostream &out) {
-    if (root == nullptr) {
-      out << ".";
-    } else {
-      out << "(";
-      display_node(root->left, out);
-      out << " " << root->elem << " ";
-      display_node(root->right, out);
-      out << ")";
+    static void display_node(const NodePointer& root, std::ostream& out) {
+        if (root == nullptr) {
+            out << ".";
+        }
+        else {
+            out << "(";
+            display_node(root->left, out);
+            out << " " << root->elem << " ";
+            display_node(root->right, out);
+            out << ")";
+        }
     }
-  }
 
-  template <typename U> static void preorder(const NodePointer &node, U func);
+    template <typename U> static void preorder(const NodePointer& node, U func);
 
-  template <typename U> static void inorder(const NodePointer &node, U func);
+    template <typename U> static void inorder(const NodePointer& node, U func);
 
-  template <typename U> static void postorder(const NodePointer &node, U func);
+    template <typename U> static void postorder(const NodePointer& node, U func);
 };
 
 template <typename T>
 template <typename U>
-void BinTree<T>::preorder(const NodePointer &node, U func) {
-  if (node != nullptr) {
-    func(node->elem);
-    preorder(node->left, func);
-    preorder(node->right, func);
-  }
+void BinTree<T>::preorder(const NodePointer& node, U func) {
+    if (node != nullptr) {
+        func(node->elem);
+        preorder(node->left, func);
+        preorder(node->right, func);
+    }
 }
 
 template <typename T>
 template <typename U>
-void BinTree<T>::inorder(const NodePointer &node, U func) {
-  if (node != nullptr) {
-    inorder(node->left, func);
-    func(node->elem);
-    inorder(node->right, func);
-  }
+void BinTree<T>::inorder(const NodePointer& node, U func) {
+    if (node != nullptr) {
+        inorder(node->left, func);
+        func(node->elem);
+        inorder(node->right, func);
+    }
 }
 
 template <typename T>
 template <typename U>
-void BinTree<T>::postorder(const NodePointer &node, U func) {
-  if (node != nullptr) {
-    postorder(node->left, func);
-    postorder(node->right, func);
-    func(node->elem);
-  }
+void BinTree<T>::postorder(const NodePointer& node, U func) {
+    if (node != nullptr) {
+        postorder(node->left, func);
+        postorder(node->right, func);
+        func(node->elem);
+    }
 }
 
 template <typename T>
 template <typename U>
 void BinTree<T>::levelorder(U func) const {
-  std::queue<NodePointer> pending;
-  if (root_node != nullptr) {
-    pending.push(root_node);
-  }
-  while (!pending.empty()) {
-    NodePointer current = pending.front();
-    pending.pop();
-    func(current->elem);
-    if (current->left != nullptr) {
-      pending.push(current->left);
+    std::queue<NodePointer> pending;
+    if (root_node != nullptr) {
+        pending.push(root_node);
     }
-    if (current->right != nullptr) {
-      pending.push(current->right);
+    while (!pending.empty()) {
+        NodePointer current = pending.front();
+        pending.pop();
+        func(current->elem);
+        if (current->left != nullptr) {
+            pending.push(current->left);
+        }
+        if (current->right != nullptr) {
+            pending.push(current->right);
+        }
     }
-  }
 }
 
 template <typename T>
-std::ostream &operator<<(std::ostream &out, const BinTree<T> &tree) {
-  tree.display(out);
-  return out;
+std::ostream& operator<<(std::ostream& out, const BinTree<T>& tree) {
+    tree.display(out);
+    return out;
 }
 
-template <typename T> BinTree<T> read_tree(std::istream &in) {
-  char c;
-  in >> c;
-  if (c == '.') {
-    return BinTree<T>();
-  } else {
-    assert(c == '(');
-    BinTree<T> left = read_tree<T>(in);
-    T elem;
-    in >> elem;
-    BinTree<T> right = read_tree<T>(in);
+template <typename T> BinTree<T> read_tree(std::istream& in) {
+    char c;
     in >> c;
-    assert(c == ')');
-    BinTree<T> result(left, elem, right);
-    return result;
-  }
+    if (c == '.') {
+        return BinTree<T>();
+    }
+    else {
+        assert(c == '(');
+        BinTree<T> left = read_tree<T>(in);
+        T elem;
+        in >> elem;
+        BinTree<T> right = read_tree<T>(in);
+        in >> c;
+        assert(c == ')');
+        BinTree<T> result(left, elem, right);
+        return result;
+    }
 }
 
 using namespace std;
 
 //@ <answer>
 //---------------------------------------------------------------
-// Modificar a partir de aquí
+// Modificar a partir de aqu�
 // --------------------------------------------------------------
 
-// Hallar la maxima diferencia entre el arbol izquierdo
-// Suponiendo que el árbol mínimo será de tamaño 2, se buscará el primer elemento del árbol izquierdo para restarlo a la raiz del árbol original y el último elemento
-// del árbol izquierdo para restarlo a la raíz del árbol derecho
-// Primer componente: Maxima diferencia del arbol correspondiente
-// Segundo componente: Elemento del árbol
-tuple<int, int, int> maxima_diferencia_aux(const BinTree<int> &arbol) {
-  if (arbol.empty())
-    return { -1, -1, -1 };
-  if (arbol.left().empty() && arbol.right().empty())
-    return { -1, arbol.root(), arbol.root() };
- 
-  auto [diffIzq, primIzq, ultIzq] = maxima_diferencia_aux(arbol.left());
-  cout << "Diff: "<< diffIzq << endl;
-  int prim = diffIzq != -1 ? arbol.root() : primIzq;
-  int dif_izq = abs(prim - arbol.root());
-  cout << "Diff_izq: " << dif_izq << endl;
-  auto [diffDer, primDer, ultDer] = maxima_diferencia_aux(arbol.right());
-  int ult = diffDer != -1 ? arbol.root() : ultDer;
-  int dif_der = abs(ult - prim);
-  cout << "Diff_der: " << dif_der << endl;
-  cout << "Arbol root: " << arbol.root() << " Prim: " << prim << " Ult: " << ult << endl;
-  return { max(prim, ult), prim, ult };
+bool es_hoja(const BinTree<int>& t) {
+    return (!t.empty()) && t.right().empty() && t.left().empty();
 }
 
-// No olvides el coste!
-// Coste O(N) siendo N el número de nodos del arbol
-int maxima_diferencia(const BinTree<int> &t) {
-  return get<0>(maxima_diferencia_aux(t));
+pair<int, int> maxDif_ultNodo(const BinTree<int>& t) {
+    if (es_hoja(t)) {
+        return{ 0, t.root() };
+    }
+    else {
+        if (t.left().empty()) {
+            auto [maxDDer, ult] = maxDif_ultNodo(t.right());
+            int maxD = max(maxDDer, abs(t.root() - t.right().root()));
+            return{ maxD, ult };
+        }
+        else if (t.right().empty()) {
+            auto [maxDIz, ult] = maxDif_ultNodo(t.left());
+            int maxD = max(maxDIz, abs(t.root() - t.left().root()));
+            return{ maxD, ult };
+        }
+        else {
+            auto [maxDIz, ultIz] = maxDif_ultNodo(t.left());
+            auto [maxDDer, ultDer] = maxDif_ultNodo(t.right());
+
+            int maxDHijos = max(maxDIz, maxDDer);
+            int maxD = max(abs(ultIz - t.right().root()), abs(t.root() - t.left().root()));
+            maxD = max(maxDHijos, maxD);
+
+            return{ maxD, ultDer };
+        }
+    }
 }
 
-// Función para tratar un caso de prueba
+// Coste lineal con respecto al n�mero de nodos de t
+int maxima_diferencia(const BinTree<int>& t) {
+    return maxDif_ultNodo(t).first;
+}
+
+// Funci�n para tratar un caso de prueba
 void tratar_caso() {
-  BinTree<int> t = read_tree<int>(cin);
-  cout << maxima_diferencia(t) << "\n";
+    BinTree<int> t = read_tree<int>(cin);
+    cout << maxima_diferencia(t) << "\n";
 }
 
 //---------------------------------------------------------------
-// No modificar por debajo de esta línea
+// No modificar por debajo de esta l�nea
 // --------------------------------------------------------------
 //@ </answer>
 
 
 int main() {
 #ifndef DOMJUDGE
-  std::ifstream in("sample.in");
-  auto cinbuf = std::cin.rdbuf(in.rdbuf());
+    std::ifstream in("sample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
-  int num_casos;
-  cin >> num_casos;
-  
-  for (int i = 0; i < num_casos; i++) {
-    tratar_caso();
-  }
+    int num_casos;
+    cin >> num_casos;
+
+    for (int i = 0; i < num_casos; i++) {
+        tratar_caso();
+    }
 
 #ifndef DOMJUDGE
-  std::cin.rdbuf(cinbuf);
-  // Descomentar si se trabaja en Windows
-  // system("PAUSE");
+    std::cin.rdbuf(cinbuf);
+    // Descomentar si se trabaja en Windows
+    // system("PAUSE");
 #endif
-  return 0;
+    return 0;
 }
 
