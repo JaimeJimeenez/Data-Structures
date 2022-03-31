bc16ad61a5a20070caf07bbb27b5292b30af6591
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-31 13:30:39 +0200

    Added Exercise 44

--- Diccionaries/46/solution.cpp
+++ Diccionaries/46/solution.cpp
@@ -9,48 +9,47 @@
 using namespace std;
 
 void aMinusculas(string& palabra) {
-    for (char c : palabra)
-        c = tolower(c);
+  for (char c : palabra)
+    c = tolower(c);
 }
 
 bool tratar_caso() {
-    int num_lineas;
-    cin >> num_lineas;
-    if (num_lineas == 0) return false;
+  int num_lineas;
+  cin >> num_lineas;
+  if (num_lineas == 0) return false;
+  
+  cin.ignore();
 
-    cin.ignore();
+  MapTree<string, vector<int>> diccionario;
+  int i = 1;
+  while (num_lineas--) {
+    string linea, palabra;
+    getline(cin, linea);
 
-    int i = 1;
-    MapTree<string, vector<int>> diccionario;
-    while (num_lineas--) {
-        string linea;
-        getline(cin, linea);
+    stringstream flujo(palabra);
 
-        
-        string palabra;
-        stringstream flujo(linea);
+    while (flujo >> palabra) {
+      aMinusculas(palabra);
 
-        while (flujo >> palabra) {
-            aMinusculas(palabra);
-
-            if (palabra.length() > 2) {
-                vector<int>& lista_linea = diccionario[palabra];
-                if (lista_linea.empty() || lista_linea.back() != i)
-                    lista_linea.push_back(i);
-            }
-            i++;
-        }
+      if (palabra.length() > 2) {
+        vector<int>& lista_linea = diccionario[palabra];
+        if (lista_linea.empty() || lista_linea.back() != i)
+          lista_linea.push_back(i);
+      }
     }
+    i++;
+  }
 
-    for (const auto& it : diccionario) {
-        auto list = it.value;
+  for (const auto & it : diccionario) {
+    auto list = it.value;
 
-        cout << it.key << " ";
-        for (auto elem : list)
-            cout << elem << " ";
-        cout << endl;
-    }
-    return true;
+    cout << it.key << " ";
+    for (auto elem : list)
+      cout <<< elem << " ";
+    cout << endl;
+  }
+
+  return true;
 }
 
 
