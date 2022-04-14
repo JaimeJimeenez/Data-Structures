c2b2d8f5fc000ce16863f0eba62b1cd68c9f6d9f
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-04-14 14:13:02 +0200

    Exercise 40 corrected by judge

--- Trees/40/solution.cpp
+++ Trees/40/solution.cpp
@@ -1,141 +1,38 @@
-/*
- * ---------------------------------------------------
- *                ESTRUCTURAS DE DATOS
- * ---------------------------------------------------
- *              Facultad de Informática
- *         Universidad Complutense de Madrid
- * ---------------------------------------------------
- */
- 
-/*
- * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
- * modificar el código contenido entre las etiquetas <answer>
- * y </answer>. Toda modificación fuera de esas etiquetas está
- * prohibida, pues no se tendrá en cuenta para la corrección.
- *
- * Tampoco esta permitido modificar las líneas que contienen
- * las etiquetas <answer> y </answer>, obviamente :-)
- */
-
 #include <iostream>
-#include <cassert>
 #include <fstream>
-#include <memory>
-#include <utility>
-#include <tuple>
-#include <algorithm>
-#include <string>
 
-template <class T> class BinTree {
-public:
-  BinTree() : root_node(nullptr) {}
-
-  BinTree(const T &elem)
-      : root_node(std::make_shared<TreeNode>(nullptr, elem, nullptr)) {}
-
-  BinTree(const BinTree &left, const T &elem, const BinTree &right)
-      : root_node(std::make_shared<TreeNode>(left.root_node, elem,
-                                             right.root_node)) {}
-
-  bool empty() const { return root_node == nullptr; }
-
-  const T &root() const {
-    assert(root_node != nullptr);
-    return root_node->elem;
-  }
-
-  BinTree left() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->left;
-    return result;
-  }
-
-  BinTree right() const {
-    assert(root_node != nullptr);
-    BinTree result;
-    result.root_node = root_node->right;
-    return result;
-  }
-
-  void display(std::ostream &out) const { display_node(root_node, out); }
-
-private:
-  // Las definiciones de TreeNode y NodePointer dependen recursivamente
-  // la una de la otra. Por eso declaro 'struct TreeNode;' antes de NodePointer
-  // para que el compilador sepa, cuando analice la definición de NodePointer,
-  // que TreeNode va a ser definida más adelante.
-
-  struct TreeNode;
-  using NodePointer = std::shared_ptr<TreeNode>;
-
-  struct TreeNode {
-    TreeNode(const NodePointer &left, const T &elem, const NodePointer &right)
-        : elem(elem), left(left), right(right) {}
-
-    T elem;
-    NodePointer left, right;
-  };
-
-  NodePointer root_node;
-
-  static void display_node(const NodePointer &root, std::ostream &out) {
-    if (root == nullptr) {
-      out << ".";
-    } else {
-      out << "(";
-      display_node(root->left, out);
-      out << " " << root->elem << " ";
-      display_node(root->right, out);
-      out << ")";
-    }
-  }
-};
-
-template <typename T>
-std::ostream &operator<<(std::ostream &out, const BinTree<T> &tree) {
-  tree.display(out);
-  return out;
-}
-
-template <typename T> BinTree<T> read_tree(std::istream &in) {
-  char c;
-  in >> c;
-  if (c == '.') {
-    return BinTree<T>();
-  } else {
-    assert(c == '(');
-    BinTree<T> left = read_tree<T>(in);
-    T elem;
-    in >> elem;
-    BinTree<T> right = read_tree<T>(in);
-    in >> c;
-    assert(c == ')');
-    BinTree<T> result(left, elem, right);
-    return result;
-  }
-}
+#include "BinTree.h"
 
 using namespace std;
 
-//@ <answer>
-//---------------------------------------------------------------
-// Modificar a partir de aquí
-// --------------------------------------------------------------
+void perfilIzqAux(BinTree<int> const& arbol, int alt, int& altMax, vector<int>& perfil) {
+  if (arbol.empty()) return;
 
+  alt++;
+  if (alt > altMax) {
+    altMax = alt;
+    perfil.push_back(arbol.root());
+  }
 
-// Función que trata un caso de prueba
-void tratar_caso() {
-  BinTree<char> t = read_tree<char>(cin);
-  
+  perfilIzqAux(arbol.left(), alt, altMax, perfil);
+  perfilIzqAux(arbol.right(), alt, altMax, perfil);
 }
 
+vector<int> perfilIzq(BinTree<int> const& arbol) {
+  vector<int> perfil;
+  int altMax = 0;
+  
+  perfilIzqAux(arbol, 0, altMax, perfil);
+  return perfil;
+}
 
-//---------------------------------------------------------------
-// No modificar por debajo de esta línea
-// --------------------------------------------------------------
-//@ </answer>
+void tratar_caso() {
+  BinTree<int> t = read_tree<int>(cin);
+  vector<int> perfil = perfilIzq(t);
 
+  for (auto elem : perfil) cout << elem << " ";
+  cout << endl;
+}
 
 int main() {
 #ifndef DOMJUDGE
